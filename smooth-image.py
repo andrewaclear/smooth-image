fcee1d4ca6a57a29a50a5e9c0a81c1f10a994369
Andrew D'Amario <51063733+andrewaclear@users.noreply.github.com>
2023-05-08 12:17:41 -0400

    add number of threads as a command line arg

--- smooth-image.py
+++ smooth-image.py
@@ -1,24 +1,18 @@
 import imageio.v3 as iio
 import numpy as np
-# import numba as nb
 import sys
 from multiprocessing.pool import Pool
 from datetime import datetime
 
-NUM_THREADS = 8
-
-# @nb.jit #numba the function
 def colour_dist(colour1: list[int], colour2: list[int]) -> int:
   dr = abs(colour1[0] - colour2[0]);
   dg = abs(colour1[1] - colour2[1]);
   db = abs(colour1[2] - colour2[2]);
   return dr + dg + db
 
-# @nb.jit #numba the function
 def is_valid(i: int, j: int, im: list[list[int]]) -> bool:
   return (0 <= i < len(im) and 0 <= j < len(im[0]))
 
-# @nb.jit #numba the function
 def avg_colour(row: int, col: int, cur_colour: list[int], colours: list[list[int]]) -> list[int]:
   r_avg = 0
   g_avg = 0
@@ -35,7 +29,6 @@ def avg_colour(row: int, col: int, cur_colour: list[int], colours: list[list[int
   avg = [r_avg//n, g_avg//n, b_avg//n]
   return avg if (colour_dist(avg, cur_colour) <= threshold and d < threshold*n and n > area/1.3) else cur_colour
 
-# @nb.jit #numba the function
 def smooth_colour(im: list[list[int]], row: int, col: int, radius: int, threshold: int) -> list[int]:
   cur_colour = im[row][col]
   colours = []
@@ -50,12 +43,10 @@ def smooth_colour(im: list[list[int]], row: int, col: int, radius: int, threshol
           colours.append(im[i][j]+[r**2+c**2]+[d])
   return avg_colour(row, col, cur_colour, colours)
 
-# @nb.jit #numba the function
 def process_pixel(point: tuple) -> tuple:
   return (point[0], point[1], smooth_colour(im, point[0], point[1], radius, threshold))
 
-# @nb.jit #numba the function
-def smooth_image():
+def smooth_image(num_threads: int):
   rows = np.zeros(shape=(height*width, 2), dtype="int32")
   for i in range(height*width):
     rows[i] = (i//width, i%width)
@@ -63,7 +54,7 @@ def smooth_image():
   processed = 0
   n = (len(im)*len(im[0]))
 
-  with Pool(NUM_THREADS) as pool:
+  with Pool(num_threads) as pool:
     for result in pool.imap_unordered(process_pixel, rows):
       im_out[result[0]][result[1]] = result[2]
       processed += 1
@@ -75,14 +66,15 @@ def smooth_image():
 
 
 if __name__ == "__main__": 
-  if (len(sys.argv) < 4):
-    print("Usage: python smooth-image.py IMAGE(.png recommended) RADIUS THRESHOLD")
+  if (len(sys.argv) < 5):
+    print("Usage: python smooth-image.py IMAGE(.png recommended) RADIUS THRESHOLD NUM_THREADS")
     exit(1)
 
   file = str(sys.argv[1])
   radius = int(sys.argv[2])
   threshold = int(sys.argv[3])
   area = (radius * 2 + 1)**2
+  num_threads = int(sys.argv[4])
 
   im = iio.imread(file).tolist()
   height = len(im)
@@ -90,7 +82,7 @@ if __name__ == "__main__":
   im_out = np.zeros(shape=(height, width, 3), dtype="uint8")
 
   start = datetime.now()
-  smooth_image()
+  smooth_image(num_threads)
   done = datetime.now()
   
   new_filename = file[:file.index('.')]+'-smooth'+file[file.index('.'):]
